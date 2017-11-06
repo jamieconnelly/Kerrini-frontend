507571e8cec462f4c21a1a96dbb96c1a879acc7e
jamieconnelly <jamiebruceconnelly@gmail.com>
2017-11-06 21:28:02 +0100

    :truck: re-organise app layout

---
+++ app/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+import { rootReducer } from './reducers/root.reducer'
+
+const root = document.getElementById('app')
+
+const configureStore = () => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(
+      thunkMiddleware,
+    ))
+  )
+  return store
+}
+
+  const store = configureStore()
+
+const Home = () => (
+  <div>Hello</div>
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Home />
+  </Provider>,
+  root,
+)
--- app/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// import style from './main.scss'
-
-ReactDOM.render(<h1>Kerrini</h1>, document.getElementById('app'))
