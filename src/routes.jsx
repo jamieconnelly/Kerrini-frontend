695006bcb4ef3dac2d1f62110115131363761c6f
jamieconnelly <jamiebruceconnelly@gmail.com>
2017-11-16 22:04:36 +0100

    :sparkles: test fetch with nginx proxy

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,9 +3,33 @@ import { Switch, Route } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
 import { Provider } from 'react-redux'
 
-const Home = () => (
-  <div>Hello</div>
-)
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { data: null }
+  }
+
+  componentDidMount() {
+    fetch('/auth/register', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'usersname',
+        email: 'usersname',
+        password: 'usersname',
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => this.setState({ data }))
+  }
+
+  render() {
+    return (<div>{this.state.data ? this.state.data : 'Hello'}</div>)
+  }
+}
 
 const SignUp = () => (
   <div>
